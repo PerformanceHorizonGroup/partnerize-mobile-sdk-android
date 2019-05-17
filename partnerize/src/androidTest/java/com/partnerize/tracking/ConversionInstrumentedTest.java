3b97f4eaf2f9ffd1ab293b1100cc2dc4ae9e29b5
Mark Gee <mark.gee@performancehorizon.com>
2019-05-17 14:53:23 +0000

    Feature/ci

--- partnerize/src/androidTest/java/com/partnerize/tracking/ConversionInstrumentedTest.java
+++ partnerize/src/androidTest/java/com/partnerize/tracking/ConversionInstrumentedTest.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2019 Performance Horizon Group LTD
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.partnerize.tracking;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Parcel;
 
@@ -7,10 +25,31 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Describes instrumented tests for conversions
+ */
 public class ConversionInstrumentedTest {
+
+    @Test
+    public void testIntent() {
+        String clickReference = "click_reference";
+
+        Intent intent = new Intent();
+        intent.setData(new Uri.Builder()
+                .scheme("https")
+                .authority("example.com")
+                .appendQueryParameter("app_clickref", clickReference)
+                .build());
+
+        Conversion conversion = new Conversion(intent);
+
+        assertEquals(clickReference, conversion.getClickRef());
+    }
+
     @Test
     public void testParcel() {
         Conversion conversion = new Conversion.Builder("click_reference")
+                .addConversionItem(new ConversionItem.Builder("19.99", "Shoes").build())
                 .build();
 
         Parcel parcel = Parcel.obtain();
