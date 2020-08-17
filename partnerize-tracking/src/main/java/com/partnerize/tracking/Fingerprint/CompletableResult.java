ac57a68d06d164038b665737eeb482d375adc50e
Mike Flowers <mike.flowers@komododigital.co.uk>
2020-08-17 10:30:55 +0100

    revert fingerprints until further notice

--- partnerize-tracking/src/main/java/com/partnerize/tracking/Fingerprint/CompletableResult.java
+++
@@ -1,5 +0,0 @@
-package com.partnerize.tracking.Fingerprint;
-
-public interface CompletableResult<T> {
-    void complete(T result);
-}
--- partnerize-tracking/src/main/java/com/partnerize/tracking/Fingerprint/FingerprintCompletable.java
+++
@@ -1,7 +0,0 @@
-package com.partnerize.tracking.Fingerprint;
-
-import com.partnerize.tracking.Completable;
-
-public interface FingerprintCompletable extends Completable {
-    void fail(FingerprintException ex);
-}
